bdb4843639de26670fbe252baaa0814a70154a85
pitraadhi <pitra.adhi@gmail.com>
2020-12-07 15:12:29 +0600

    add batununggal branch and pricelist4.php

---
+++ config/pricelist4.php
@@ -0,0 +1,20 @@
+<?php 
+
+return [
+	'carwash' => 35000,
+	'wax' => 65000,
+	'spraywax' => 50000,
+	'softcoating' => 175000,
+	'fogging' => 35000,
+	'bikewash' => 20000,
+	'detailing' => 750000,
+	'fnb' => 6000,
+	'spraywax_coret' => 'Rp 100.000',
+	'spraywax_promo' => 'Rp 60.000',
+	'creamwax_coret' => 'Rp 125.000',
+	'creamwax_promo' => 'Rp 80.000',
+	'softcoating_coret' => 'Rp 350.000',
+	'softcoating_promo' => 'Rp 215.000',
+	'detailing_coret' => '5%',
+	'detailing_promo' => '10%'
+];
