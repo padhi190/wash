e03fb24101f658a5f3859a8b0a779bd0b7c71dfa
pitraadhi <pitra.adhi@gmail.com>
2020-12-11 18:57:08 +0600

    change BN pricelist

--- config/pricelist4.php
+++ config/pricelist4.php
@@ -1,20 +1,20 @@
 <?php 
 
 return [
-	'carwash' => 35000,
-	'wax' => 65000,
-	'spraywax' => 50000,
+	'carwash' => 50000,
+	'wax' => 50000,
+	'spraywax' => 30000,
 	'softcoating' => 175000,
 	'fogging' => 35000,
 	'bikewash' => 20000,
 	'detailing' => 750000,
 	'fnb' => 6000,
 	'spraywax_coret' => 'Rp 100.000',
-	'spraywax_promo' => 'Rp 60.000',
+	'spraywax_promo' => 'Rp 70.000',
 	'creamwax_coret' => 'Rp 125.000',
-	'creamwax_promo' => 'Rp 80.000',
+	'creamwax_promo' => 'Rp 90.000',
 	'softcoating_coret' => 'Rp 350.000',
-	'softcoating_promo' => 'Rp 215.000',
+	'softcoating_promo' => 'Rp 225.000',
 	'detailing_coret' => '5%',
 	'detailing_promo' => '10%'
 ];
